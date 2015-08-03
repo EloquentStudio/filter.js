f1bb383bb12efcb3514458d640913e6ddeb03a18
Jiren <jirenpatel@gmail.com>
2015-08-03 10:01:48 +0530

    Added pagination example

--- pagination.js
+++ pagination.js
@@ -7,15 +7,37 @@ $(document).ready(function(){
     console.log(record)
   };
 
+  var afterFilter = function(result, jQ){
+    $('#total_movies').text(result.length);
+    var checkboxes  = $("#genre_criteria :input:gt(0)");
+    var qResult = JsonQuery(result);
+
+    checkboxes.each(function(){
+      var c = $(this), count = 0
+
+      if(result.length > 0){
+        count = qResult.where({ 'genre': c.val() }).count;
+      }
+      c.next().text(c.val() + '(' + count + ')')
+    });
+  }
+
   var FJS = FilterJS(movies, '#movies', {
     template: '#movie-template',
-    search: {ele: '#searchbox'},
+    search: { ele: '#searchbox' },
     //search: {ele: '#searchbox', fields: ['runtime']}, // With specific fields
     callbacks: {
-      afterFilter: function(result){
-        $('#total_movies').text(result.length);
-      }
+      afterFilter: afterFilter 
+    },
+    pagination: {
+      container: '#pagination',
+      visiblePages: 5,
+      perPage: {
+        values: [12, 15, 18],
+        container: '#per_page'
+      },
     }
+
     //appendToContainer: appendToContainer
   });
 
@@ -35,11 +57,13 @@ $(document).ready(function(){
     }
   });
 
+  /*
   FJS.setStreaming({
     data_url: 'data/stream_movies.json',
     stream_after: 1,
     batch_size: 50
   });
+  */
 
   FJS.addCriteria({field: 'year', ele: '#year_filter', type: 'range', all: 'all'});
   FJS.addCriteria({field: 'rating', ele: '#rating_filter', type: 'range'});
